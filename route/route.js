d94854bb7666eba41a2c86143bee2bb07d76c25c
unknown <akitl1@yahoo.fr>
2018-03-20 21:46:57 +0100

    api bof

---
+++ route/route.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var users = require('../controllers/users');
+
+  // todoList Routes
+  app.route('/tasks')
+    .get(users.list_all_tasks);
+    //.post(todoList.create_a_task);
+
+
+  /*app.route('/tasks/:taskId')
+    .get(todoList.read_a_task)
+    .put(todoList.update_a_task)
+    .delete(todoList.delete_a_task); */
+}; 
